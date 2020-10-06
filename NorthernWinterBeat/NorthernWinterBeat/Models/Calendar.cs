6077e4d3e2d0c518ee40f8891e6c59394485a798
martin pinholt <martinpinholthansen@hotmail.com>
2020-10-06 12:45:36 +0200

    sdfesf

---
+++ NorthernWinterBeat/NorthernWinterBeat/Models/Calendar.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Calendar
+{
+	public Calendar()
+	{
+	}
+
+    private List<Concert> Concerts = new List<Concert>(); 
+        
+
+	public void AddConcert(Concert concert)
+    {
+        Concerts.Add(concert);
+    }
+
+    public void RemoveConcert(Concert concert)
+    {
+        Concerts.Remove(concert);
+    }
+}
+
+
