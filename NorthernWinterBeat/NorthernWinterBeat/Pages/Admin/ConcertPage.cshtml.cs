e59d903d6c16cc6c1c57ed0fbc14cf3bdedca040
goldflue <45980653+goldflue@users.noreply.github.com>
2020-10-12 10:52:11 +0200

    Updated ConcertPageModel

--- NorthernWinterBeat/NorthernWinterBeat/Pages/Admin/ConcertPage.cshtml.cs
+++ NorthernWinterBeat/NorthernWinterBeat/Pages/Admin/ConcertPage.cshtml.cs
@@ -9,7 +9,7 @@ namespace NorthernWinterBeat.Pages.Admin
 {
     public class ConcertPageModel : PageModel
     {
-        public Concert concert { get; private set; } = new Concert( new DateTime(2021, 01, 28, 20, 00, 00), new DateTime(2021, 01, 28, 20, 30, 00), new Models.Venue("Studenterhuset", "Borgergase 2", 33), "Martin Hansen", "Fuckig sejg"),
+        public Concert concert { get; private set; } = new Concert(new DateTime(2021, 01, 28, 20, 00, 00), new DateTime(2021, 01, 28, 20, 30, 00), new Models.Venue("Studenterhuset", "Borgergase 2", 33), "Martin Hansen", "Fuckig sejg");
         public void OnGet()
         {
         }
