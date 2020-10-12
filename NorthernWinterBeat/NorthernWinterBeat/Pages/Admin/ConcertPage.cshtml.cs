d1582c0009891206ad967deede4c1178309dccea
goldflue <45980653+goldflue@users.noreply.github.com>
2020-10-12 10:48:27 +0200

    began work on fixes

--- NorthernWinterBeat/NorthernWinterBeat/Pages/Admin/ConcertPage.cshtml.cs
+++ NorthernWinterBeat/NorthernWinterBeat/Pages/Admin/ConcertPage.cshtml.cs
@@ -9,13 +9,7 @@ namespace NorthernWinterBeat.Pages.Admin
 {
     public class ConcertPageModel : PageModel
     {
-        public List<Concert> Concerts { get; set; } = new List<Concert>() {
-            new Concert( new DateTime(2021, 01, 28, 20, 00, 00), new DateTime(2021, 01, 28, 20, 30, 00), new Models.Venue("Studenterhuset", "Borgergase 2", 33), "Martin Hansen", "Fuckig sejg"),
-            new Concert( new DateTime(2021, 01, 28, 21, 00, 00), new DateTime(2021, 01, 28, 21, 30, 00), new Models.Venue("Studenterhuset", "Borgergase 2", 36), "ANders Hansen", "Fuckig sejg"),
-            new Concert( new DateTime(2021, 01, 28, 19, 00, 00), new DateTime(2021, 01, 28, 19, 30, 00), new Models.Venue("Studenterhuset", "Borgergase 2", 38), "Martsefs Hansen", "Fuckig sejg"),
-            new Concert( new DateTime(2021, 01, 28, 19, 45, 00), new DateTime(2021, 01, 28, 20, 45, 00), new Models.Venue("Studenterhuset", "Borgergase 2", 30), "Marsefsetin Hansen", "Fuckig sejg") };
-        [BindProperty (SupportsGet =true)]
-        public int ConcertID { get; set; }
+        public Concert concert { get; private set; } = new Concert( new DateTime(2021, 01, 28, 20, 00, 00), new DateTime(2021, 01, 28, 20, 30, 00), new Models.Venue("Studenterhuset", "Borgergase 2", 33), "Martin Hansen", "Fuckig sejg"),
         public void OnGet()
         {
         }
