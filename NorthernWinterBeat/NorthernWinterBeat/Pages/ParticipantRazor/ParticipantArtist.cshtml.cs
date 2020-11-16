2ce2f056c731da875e12ccd172e02889869e61b1
martin pinholt <martinpinholthansen@hotmail.com>
2020-11-16 13:48:27 +0100

    Fjerne booking

--- NorthernWinterBeat/NorthernWinterBeat/Pages/ParticipantRazor/ParticipantArtist.cshtml.cs
+++ NorthernWinterBeat/NorthernWinterBeat/Pages/ParticipantRazor/ParticipantArtist.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using NorthernWinterBeat.Models;
 using NorthernWinterBeatLibrary.Managers;
 
 namespace NorthernWinterBeat.Pages.ParticipantRazor
@@ -24,9 +25,7 @@ namespace NorthernWinterBeat.Pages.ParticipantRazor
         }
         public void OnGet(int id = 0)
         {
-
             concert = FestivalManager.Calendar.GetConcert(id);
-
         }
 
         public IActionResult OnPostBookSpot(int id)
@@ -40,6 +39,16 @@ namespace NorthernWinterBeat.Pages.ParticipantRazor
         public IActionResult OnPostGoBack(DateTime Day)
         {
             return RedirectToPage("./ParticipantConcertOverview", new { Day = Day });
+        }
+        public IActionResult OnPostRemoveBooking(int id)
+        {
+            var claimTicketID = HttpContext.User.Claims.Where(c => c.Type == "TicketID").Select(t => t.Value).First();
+
+            Concert c = FestivalManager.Calendar.GetConcert(id);
+            Participant p = FestivalManager.GetParticipants().Where(p => p.Ticket?.TicketNumber == claimTicketID).First();
+            Booking b = c.Bookings.Where(b => b.Participant == p).FirstOrDefault();
+            c.RemoveBooking(b);
+            return RedirectToPage("./ParticipantArtist", new { id = id });
         }
     }
 }
